4d2a823eeb796d622aa0656d57ee12f1063aae01
Garrett Gunnell <garrett99@bendbroadband.com>
2019-09-30 22:35:34 -0700

    reads stop_word file and adds stop words to list.

--- Exercise1/exercise1.py
+++ Exercise1/exercise1.py
@@ -1,1 +1,18 @@
 
+stop_words_file = open("stop_words.txt", "r");
+
+stop_words_lines = stop_words_file.readlines();
+
+stop_words = [];
+
+for line in stop_words_lines:
+    word = '';
+    for letter in range(len(line)):
+        if (letter + 1) != len(line) and line[letter] != ',':
+            word += line[letter];
+            if (line[letter + 1] == ','):
+                stop_words.append(word);
+                word = '';
+
+for i in stop_words:
+    print(i)
